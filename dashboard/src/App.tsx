970837ad49f1be07b772fc66b878fd54a9a7d226
rubel <ahiqur35-611@diu.edu.bd>
2024-10-21 15:23:37 +0600

    shadcn setup

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1>Hello</h1>
+      <Button>Click me</Button>
     </>
   )
 }
