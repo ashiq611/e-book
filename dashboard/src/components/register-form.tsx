78b61933b34d68b632ff7b49d7fb8ab64dad9022
rubel <ahiqur35-611@diu.edu.bd>
2024-10-21 22:45:58 +0600

    register form ready

---
+++ dashboard/src/components/register-form.tsx
@@ -0,0 +1,58 @@
+
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Link } from "react-router-dom"
+
+export function RegisterForm() {
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your details to create your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" type="text" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Already have an account?
+          <Link to="/signup" className="underline">
+            Sign in
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ dashboard/src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from '@/components/register-form'
+import React from 'react'
+
+function RegisterPage() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <RegisterForm/>
+    </div>
+  )
+}
+
+export default RegisterPage
